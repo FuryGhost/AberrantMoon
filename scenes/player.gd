5751a46f11c662ebf0d73fccca0aad718afb8e04
FuryGhost <78565819+FuryGhost@users.noreply.github.com>
2024-03-02 20:41:15 +0200

    Added bunny art and animation (#3)

--- scenes/player.gd
+++ scenes/player.gd
@@ -19,12 +19,24 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("move_left", "move_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0;
+		$AnimatedSprite2D.play("run")
 	else:
 		if is_on_floor():
 			velocity.x = 0
+			$AnimatedSprite2D.play("default_sitting")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-
+	
+	if !is_on_floor():
+		if velocity.y > -100 && velocity.y < 100:
+			$AnimatedSprite2D.play("mid_jump")
+		elif velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
+	
 	move_and_slide()
