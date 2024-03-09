52c10ec2b3543d9e373239a8e78f960743709d4e
Ari Benditkis <maria.benditkis@gmail.com>
2024-03-09 19:17:28 +0200

    Refactored player code

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,6 +11,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Private variables
 var _jump_boost_direction = 0
 
+# @onready variables
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	floor_stop_on_slope = true
@@ -19,53 +22,69 @@ func _ready():
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
+	var on_floor: bool = is_on_floor()
 	var direction = Input.get_axis("move_left", "move_right")
 	
+	_handle_gravity(delta, on_floor)
+	
+	_handle_horizontal_move(delta, on_floor, direction)
+	
+	_handle_jump(on_floor, direction)
+	
+	_handle_rotation(on_floor)
+	
+	_handle_animation(on_floor, direction)
+	
+	move_and_slide()
+
+func _handle_gravity(delta: float, on_floor: bool):
+	if not on_floor:
+		velocity.y += gravity * delta
+
+func _handle_horizontal_move(delta: float, on_floor: bool, direction: float):
 	if direction:
 		velocity.x = direction * SPEED
 		
-		if direction == _jump_boost_direction && !is_on_floor():
+		if direction == _jump_boost_direction && !on_floor:
 			velocity.x += direction * JUMP_BOOST_SPEED
 		else:
 			_jump_boost_direction = 0
-		
-		$AnimatedSprite2D.flip_h = direction < 0;
-		$AnimatedSprite2D.play("run")
 	else:
-		if is_on_floor():
+		if on_floor:
 			velocity.x = 0
-			$AnimatedSprite2D.play("default_sitting")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 		
 		_jump_boost_direction = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+func _handle_jump(on_floor: bool, direction: float):
+	if Input.is_action_just_pressed("jump") and on_floor:
 		velocity.y = JUMP_VELOCITY
 		
 		if direction:
 			velocity.x += direction * JUMP_BOOST_SPEED
 			_jump_boost_direction = direction
-	
-	if !is_on_floor():
+
+func _handle_rotation(on_floor: bool):
+	if !on_floor:
 		rotation = 0
-		
-		if velocity.y > -100 && velocity.y < 100:
-			$AnimatedSprite2D.play("mid_jump")
-		elif velocity.y < 0:
-			$AnimatedSprite2D.play("jump")
-		else:
-			$AnimatedSprite2D.play("fall")
 	else:
-		rotation = get_floor_angle()
-		
-		if get_floor_normal().x < 0:
-			rotation *= -1
+		var rotation_direction = 1 if get_floor_normal().x >= 0 else -1
+		rotation = get_floor_angle() * rotation_direction
+
+func _handle_animation(on_floor: bool, direction: float):
+	if direction:
+		animated_sprite.flip_h = direction < 0
 	
-	move_and_slide()
+	if on_floor:
+		if direction:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("default_sitting")
+	else:
+		if velocity.y > -100 && velocity.y < 100:
+			animated_sprite.play("mid_jump")
+		elif velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
