61943196558ecc0dbda4e2139c9554933f1512ad
Ari Benditkis <maria.benditkis@gmail.com>
2024-03-10 13:09:22 +0200

    Added camera movement

--- scenes/main.gd
+++ scenes/main.gd
@@ -12,3 +12,7 @@ func _process(delta):
 	if (Input.is_action_pressed("ui_cancel")):
 		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 		get_tree().quit()
+
+
+func _on_level_camera_move(new_position):
+	$Camera2D.position = new_position
